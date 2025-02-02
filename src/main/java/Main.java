6a60419c5e0f54e010945214933e163b3d63a4cf
Kristina0610 <gaevskaya-2011@mail.ru>
2025-02-02 21:51:30 +0300

    Изменение логики работы Main.java - выполнение задания по теме "Циклы"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,36 @@
+package tasks;
+
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        //Счётчик верно указанных путей
+        int validFileCount = 0;
+
+        // Бесконечный цикл
+        while (true) {
+            // Запрос пути к файлу
+            System.out.print("Введите путь к файлу: ");
+
+            //Запрос пути в консоли можно таким образом:
+            String path = new Scanner(System.in).nextLine();
+
+            //Определение, существует ли файл, путь к которому был указан
+            File file = new File(path);
+            boolean fileExists = file.exists();
+
+            //Определение, является ли указанный путь путём именно к файлу, а не к папке
+            boolean isDirectoty = file.isDirectory();
+
+            if (!fileExists || isDirectoty) {
+                System.out.println("Файл не существует или указанный путь является путём к папке, а не к файлу");
+                continue;
+            } else {
+                validFileCount++;
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + validFileCount);
+            }
+        }
     }
 }
